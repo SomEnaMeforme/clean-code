8cb2da944070fd88922366172a7bff2d1050a0b9
SomEnaMeforme <rushkova909@gmail.com>
2024-12-29 06:42:07 +0500

    Фикс взаимодействия тегов bold и italic

--- cs/Markdown/Md.cs
+++ cs/Markdown/Md.cs
@@ -1,54 +1,38 @@
 ﻿using Markdown.Tags;
-using System.ComponentModel.Design.Serialization;
 using System.Text;
 
 namespace Markdown
 {
     public class Md
     {
-        public static readonly IReadOnlyDictionary<char, Dictionary<string, Func<string, int, Tag>>> MdTags = new Dictionary<char, Dictionary<string, Func<string, int, Tag>>>()
-        {
-            {
-                '_', new Dictionary<string, Func<string, int, Tag>>
+        public static readonly IReadOnlyDictionary<char, Dictionary<string, Func<string, int, Tag>>> MdTags =
+           new Dictionary<char, Dictionary<string, Func<string, int, Tag>>>()
+           {
                 {
-                    { "_", Italic.CreateInstance},
-                    { "__", Bold.CreateInstance}
-                }
-            },
-            {
-                '#', new Dictionary<string, Func<string, int, Tag>>
+                    '_', new Dictionary<string, Func<string, int, Tag>>
+                    {
+                        { "_", (markdown, tagStart) => new Italic(markdown, tagStart)},
+                        { "__", (markdown, tagStart) => new Bold(markdown, tagStart)}
+                    }
+                },
                 {
-                    { "#", (markdown, tagStart) => new Header(markdown, tagStart) }
-                }
-            },
-            {
-                '\\',
-                new Dictionary<string, Func<string, int, Tag>>
+                    '#', new Dictionary<string, Func<string, int, Tag>>
+                    {
+                        { "#", (markdown, tagStart) => new Header(markdown, tagStart) }
+                    }
+                },
                 {
-                    { "\\", (markdown, tagStart) => new Escape(markdown, tagStart) }
+                    '\\',
+                    new Dictionary<string, Func<string, int, Tag>>
+                    {
+                        { "\\", (markdown, tagStart) => new Escape(markdown, tagStart) }
+                    }
                 }
-            }
-        };
-
-        internal static Tag GetOpenTag(int tagStart, string markdownText, out int contextStart)
-        {           
-            var tagBegin = markdownText[tagStart];
-            var tags = MdTags[tagBegin].Keys.OrderByDescending(k => k.Length);
-            foreach (var tag in tags)
-            {
-                if (tag.Length + tagStart < markdownText.Length
-                    && markdownText.Substring(tagStart, tag.Length) == tag)
-                {
-                    contextStart = tagStart + tag.Length;
-                    return MdTags[tagBegin][tag](markdownText, tagStart);
-                }
-            }
-            throw new InvalidOperationException("Попытка получить несуществующий тег");
-        }
+           };
 
         public string Render(string markdownText)
         {
-            var parser = new MdParser(markdownText);
+            var parser = new MdParser(markdownText, MdTags);
             var tags = parser.GetTags();
             var result = new StringBuilder();
             var tagsStart = tags.ToDictionary(t => t.TagStart);
--- cs/Markdown/MdParser.cs
+++ cs/Markdown/MdParser.cs
@@ -1,11 +1,10 @@
 ﻿using Markdown.Tags;
+using Markdown.Tags.TagsInteraction;
 
 namespace Markdown;
 
-public class MdParser(string markdownText)
+public class MdParser(string markdownText, IReadOnlyDictionary<char, Dictionary<string, Func<string, int, Tag>>> tagCreators)
 {
-    private readonly MdRulesForNestedTags rules = new();
-
     private class ReaderPosition
     {
         public int Position { get; set; }
@@ -13,35 +12,38 @@ public class MdParser(string markdownText)
 
     public Tag[] GetTags()
     {
+        var tokens = Tokenize(markdownText);
         var result = new List<Tag>();
+        var possibleTags = FindCorrectTags(tokens);
+        var possibleTagsStarts = possibleTags.Select(t => t.TagStart).ToHashSet();
         var current = new ReaderPosition();
-        while (current.Position < markdownText.Length)
+        var tagsIterator = possibleTags.OrderBy(t => t.TagStart).GetEnumerator();
+        while (tagsIterator.MoveNext())
         {
-            if (Md.MdTags.ContainsKey(markdownText[current.Position]))
-            {
-                var newTag = CreateTag(current, []);
-                if (newTag.IsTagClosed)
-                    result.Add(newTag);
-            }
-            else current.Position++;
+            var newTag = CreateTag(current, [], tagsIterator, possibleTagsStarts);
+            if (newTag.IsTagClosed)
+                result.Add(newTag);
         }
         return result.ToArray();
     }
 
-    private Tag CreateTag(ReaderPosition current, List<Tag> external)
+    private Tag CreateTag(ReaderPosition current, List<Tag> external, IEnumerator<Tag> tagsIterator, HashSet<int> tagsStarts)
     {
-        var openTag = Md.GetOpenTag(current.Position, markdownText, out int contextStart);
-        current.Position = contextStart;
-        var isContextCorrect = true;
+        var openTag = tagsIterator.Current;
+        current.Position = openTag.SkipTag(openTag.TagStart);
+        var isContextCorrect = openTag.AcceptIfContextCorrect(current.Position);
         var nested = new List<Tag>();
         while (current.Position < markdownText.Length && !openTag.AcceptIfContextEnd(current.Position))
         {
-            if (Md.MdTags.ContainsKey(markdownText[current.Position]) && openTag is not Escape)
+            if (tagsStarts.Contains(current.Position))
             {
-                var newTag = CreateTag(current, [openTag]);
-                if (newTag.IsTagClosed)
+                if (tagsIterator.MoveNext())
                 {
-                    nested.Add(newTag);
+                    var newTag = CreateTag(current, [openTag], tagsIterator, tagsStarts);
+                    if (newTag.IsTagClosed)
+                    {
+                        nested.Add(newTag);
+                    }
                 }
             }
             else
@@ -51,16 +53,85 @@ public class MdParser(string markdownText)
             }
         }
 
-        if ((external.Count == 0 || rules.IsNestedTagWorks(external[^1], openTag)) && isContextCorrect)
+        if ((external.Count == 0 || Tag.IsNestedTagWorks(external[^1].TagType, openTag.TagType)) && isContextCorrect)
         {
-            openTag.TryCloseTag(current.Position, markdownText, out int tagEnd, nested);
+            openTag.TryCloseTag(current.Position, markdownText, out var tagEnd, nested);
             current.Position = tagEnd;
         }
         else
         {
-            current.Position = openTag.SkipTag(current.Position);
+            current.Position = openTag is PairTag ? openTag.SkipTag(current.Position) : current.Position;
         }
-        
+
         return openTag;
+    }
+
+    private Tag GetOpenTag(int tagStart, string markdownText, out int contextStart)
+    {
+        var tagBegin = markdownText[tagStart];
+        var tags = tagCreators[tagBegin];
+        var orderedTags = tags.Keys.OrderByDescending(k => k.Length);
+        foreach (var tag in orderedTags)
+        {
+            if (tag.Length + tagStart <= markdownText.Length
+                && markdownText.Substring(tagStart, tag.Length) == tag)
+            {
+                contextStart = tagStart + tag.Length;
+                var createOpenTag = tags[tag];
+                return createOpenTag(markdownText, tagStart);
+            }
+        }
+        throw new InvalidOperationException("Попытка получить несуществующий тег");
+    }
+
+    public List<Tag> Tokenize(string markdownText)
+    {
+        var tokens = new List<Tag>();
+        for (int i = 0; i < markdownText.Length; i++)
+        {
+            if (tagCreators.ContainsKey(markdownText[i]))
+            {
+                var tag = GetOpenTag(i, markdownText, out var contextStart);
+                tokens.Add(tag);
+                i = contextStart - 1;
+                if (tag is Escape) i++;
+            }
+        }
+        return tokens;
+    }
+
+    public List<Tag> FindCorrectTags(List<Tag> tokens)
+    {
+        var result = MatchTags(tokens);
+        var closedTags = result.Closed;
+        var unclosedPairTag = result.Unclosed;
+        closedTags.AddRange(new UnclosedPairTagsRules().Apply(unclosedPairTag.Reverse().ToList()));
+        return closedTags;
+    }
+
+    public (List<Tag> Closed, Stack<Tag> Unclosed) MatchTags(List<Tag> possibleTags)
+    {
+        var closedTags = new List<Tag>();
+        var stack = new Stack<Tag>();
+
+        for (int i = 0; i < possibleTags.Count; i++)
+        {
+            var tag = possibleTags[i];
+            if (tag is not PairTag)
+            {
+                closedTags.Add(tag);
+            }
+            else if (stack.Count == 0 || stack.Peek().TagType != tag.TagType)
+            {
+                stack.Push(tag);
+            }
+            else
+            {
+                var startTag = stack.Pop();
+
+                closedTags.Add(startTag);
+            }
+        }
+        return (closedTags, stack);
     }
 }
