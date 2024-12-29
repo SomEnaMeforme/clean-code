9008d8fbddd464db589e579e264ad3d958be55a1
SomEnaMeforme <rushkova909@gmail.com>
2024-12-29 06:40:44 +0500

    Перенос логики обработки выделения части слова в правила

--- cs/Markdown/Tags/Bold.cs
+++ cs/Markdown/Tags/Bold.cs
@@ -1,34 +1,18 @@
 ﻿namespace Markdown.Tags;
 
-public class Bold : PairTag
+public class Bold(string mdText, int tagStart) : PairTag(mdText, tagStart)
 {
-    protected Bold(string mdText, int tagStart) : base(mdText, tagStart)
-    {
-        MarkdownText = mdText;
-        TagStart = tagStart;
-    }
-
-    public static Bold CreateInstance(string markdownText, int tagStart)
-    {
-        if (tagStart > 0 && char.IsLetter(markdownText[tagStart - 1]))
-            return new BoldSelectPartWord(markdownText, tagStart);
-
-        return new BoldSelectFewWords(markdownText, tagStart);
-    }
-
-    protected string MarkdownText;
     protected override string MdTag => "__";
     protected override string HtmlTag => "strong";
 
-    public override bool AcceptIfContextEnd(int currentPosition)
-    {
-        return currentPosition != MarkdownText.Length - 1;
-    }
+    public override MdTagType TagType => MdTagType.Bold;
 
-    public override bool AcceptIfContextCorrect(int currentPosition)
+    public override void TryCloseTag(int contextEnd, string sourceMdText, out int tagEnd, List<Tag>? nested = null)
     {
-        return !((MarkdownText[currentPosition] == '_' && MarkdownText[currentPosition + 1] == '_')
-                 || char.IsDigit(MarkdownText[currentPosition])
-                 || (currentPosition == TagStart + MdTag.Length && MarkdownText[currentPosition] == ' '));
+        tagEnd = contextEnd + MdTag.Length;
+        if (contextEnd != TagStart + MdTag.Length)
+        {
+            base.TryCloseTag(contextEnd, sourceMdText, out tagEnd, nested);
+        }
     }
 }
--- cs/Markdown/Tags/Italic.cs
+++ cs/Markdown/Tags/Italic.cs
@@ -1,34 +1,10 @@
-﻿namespace Markdown.Tags;
+﻿using Markdown.Tags.ContextRules;
 
-public class Italic: PairTag
+namespace Markdown.Tags;
+
+public class Italic(string mdText, int tagStart) : PairTag(mdText, tagStart)
 {
-    protected Italic(string mdText, int tagStart) : base(mdText, tagStart)
-    {
-        MarkdownText = mdText;
-        TagStart = tagStart;
-    }
-
-    public static Italic CreateInstance(string markdownText, int tagStart)
-    {
-        if (tagStart > 0 && char.IsLetter(markdownText[tagStart - 1]))
-            return new ItalicSelectPartOfOneWord(markdownText, tagStart);
-
-        return new ItalicSelectFewWords(markdownText, tagStart);
-    }
-
-    protected string MarkdownText;
     protected override string MdTag => "_";
     protected override string HtmlTag => "em";
-
-    public override bool AcceptIfContextEnd(int currentPosition)
-    {
-        isCloseByFindCloseTag = MarkdownText[currentPosition] == '_';
-        return isCloseByFindCloseTag;
-    }
-    public override bool AcceptIfContextCorrect(int currentPosition)
-    {
-        return base.AcceptIfContextCorrect(currentPosition) && !((MarkdownText[currentPosition] == '_' && MarkdownText[currentPosition + 1] == '_')
-          || char.IsDigit(MarkdownText[currentPosition])
-          || (currentPosition == TagStart + MdTag.Length && MarkdownText[currentPosition] == ' '));
-    }
+    public override MdTagType TagType => MdTagType.Italic;
 }
